c498fa8d2df59ec20867dd23ba5b13735611857a
Liuxin4950 <liuxin4950@gmail.com>
2024-08-03 16:11:25 +0800

    试试打包

--- src/Store.js
+++ src/Store.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 
 export const useSearchStore = defineStore('search', {
     state: () => ({
-        selectedEngine: 'google',
+        selectedEngine: 'bing',
         query: ''
     }),
     actions: {
