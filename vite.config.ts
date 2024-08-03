ba9fb474bf096865fc4eccca4f99ab8524689c9f
Liuxin4950 <liuxin4950@gmail.com>
2024-08-03 16:41:28 +0800

    Update base config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mypage/', // 确保 base 配置为你的项目路径
   plugins: [
     vue(),
   ],
